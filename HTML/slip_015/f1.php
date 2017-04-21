c17399555499b47790d24c1a192c6d558c1883d2
jaydoddamani <jaydoddamani@gmail.com>
2017-04-21 09:53:03 +0530

    php

---
+++ HTML/slip_015/f1.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <?php
+            echo $_POST["ename"];
+        ?>
+        <br>
+        Your designation is:
+        <?php
+            echo $_POST["desgn"];
+        ?>
+    </body>
+</html>
