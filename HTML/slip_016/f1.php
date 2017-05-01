fc4d41c8ac4f0b9a2f78df2f73d732ad2fe09e5b
jaydoddamani <jaydoddamani@gmail.com>
2017-05-01 13:02:58 +0530

    not done.(img not linked)

---
+++ HTML/slip_016/f1.php
@@ -0,0 +1,14 @@
+<?php
+    $img1=$_GET['img'];
+?>
+<html>
+<head>
+    <style type="text/css">
+    body{
+        background-image:url('<?php echo $img1 ?>');
+    }
+    </style>
+</head>
+<body>background changed
+</body>
+</html>
