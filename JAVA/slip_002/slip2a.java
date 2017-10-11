88c4b2d7b6c8d12dab00528d88d1a37a8edd3bda
jaydoddamani <jaydoddamani@gmail.com>
2017-10-11 21:20:08 +0530

    new

--- JAVA/slip_002/slip2a.java
+++ JAVA/slip_002/slip2a.java
@@ -1,14 +1,19 @@
 import java.io.* ;
 class slip2a{
     public static void main(String[] args) throws IOException{
-        String city[]=new String[5];
+        BufferedReader no=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Number of Citys");
+        int n;
+        n=Integer.parseInt(no.readLine());
+        System.out.println("Enter Cities");
+        String city[]=new String[n];
         String t;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        for(int i=0; i<5; i++){
+        for(int i=0; i<n; i++){
             city[i]=br.readLine ();
         }
-        for(int i=0;i<5;i++){
-            for(int j=i+1; j<5; j++){
+        for(int i=0;i<n;i++){
+            for(int j=i+1; j<n; j++){
                 if(city[j].compareTo(city[i])>0) {
                     t=city[i];
                     city[i]=city[j];
@@ -16,7 +21,8 @@ class slip2a{
                 }
             }
         }
-        for(int i=0;i<5;i++){
+        System.out.println("");
+        for(int i=0;i<n;i++){
             System.out.println(city[i]);
         }
    }
--- JAVA/slip_005/student.java
+++ JAVA/slip_005/student.java
@@ -12,16 +12,12 @@ class student{
 	 snm=nm;
 	 System.out.println("Object "+i+" is created");
 	}
-	
-	public String toString()
-	{
+	public String toString(){
 	String str;
 	str=rno+""+snm;
 	return str;
 	}
-
-	public static void main(String args[])
-	{
+	public static void main(String args[]){
 	student s1=new student(1,"Jay");
 	student s2=new student(2,"Sagar");
 	student s3=new student(3,"jimmy");
